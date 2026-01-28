8be5419ba32ba5fd9e0f203c345c3a3458c626da
herbgrace <thelen.alexander05@gmail.com>
2026-01-28 12:07:12 -0700

    Added Database to the Distributed System Network/Compose. Added a setup SQL query to get database ready for the API.

---
+++ DBSetup.sql
@@ -0,0 +1,44 @@
+CREATE DATABASE IF NOT EXISTS GamesExchangeDB;
+USE GamesExchangeDB;
+
+CREATE TABLE IF NOT EXISTS `Users` (
+    id BIGINT AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    address TEXT NOT NULL,
+    UNIQUE KEY unique_username (username),
+    UNIQUE KEY unique_email (email),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS Games (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    releaseYear INT NOT NULL,
+    releaseSystem VARCHAR(100) NOT NULL,
+    `condition` ENUM('Mint', 'Good', 'Fair', 'Poor') NOT NULL,
+    previousOwner BIGINT,
+    FOREIGN KEY (previousOwner) REFERENCES `Users`(id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS Offers (
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+	gameRequested BIGINT,
+	gameOffered BIGINT,
+	`status` ENUM('Pending', 'Accepted', 'Rejected') DEFAULT 'Pending',
+	FOREIGN KEY (gameRequested) REFERENCES `Games`(id) ON DELETE CASCADE,
+	FOREIGN KEY (gameOffered) REFERENCES `Games`(id) ON DELETE CASCADE
+);
+
+INSERT INTO Users (username, email, password, address) VALUES 
+("Test Username", "example@gmail.com", "AwesomePassword", "123 Main St. USA"),
+("Another User", "sayWHAT@yahoo.com", "Secure", "Something something st.");
+
+INSERT INTO Games (`name`, publisher, releaseYear, releaseSystem, `condition`, previousOwner) VALUES
+("Super Mario Bros.", "Nintendo", 1985, "NES", "Good", 1),
+("Contra", "Konami", 1988, "NES", "Fair", 2);
+
+INSERT INTO Offers (gameRequested, gameOffered) VALUES
+(1, 2);
